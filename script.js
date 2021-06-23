0d708b2960bec0050053bbd4be90d4698f149914
Logan Parker <84515590+Logan-Parker@users.noreply.github.com>
2021-06-23 16:53:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function red()
+{
+  document.getElementById('paragraph').style.color="red"
+}
+
+function orange()
+{
+  document.getElementById('paragraph').style.color="orange"
+}
+
+function yellow()
+{
+  document.getElementById('paragraph').style.color="yellow"
+}
+
+function green()
+{
+  document.getElementById('paragraph').style.color="green"
+}
+
+function blue()
+{
+  document.getElementById('paragraph').style.color="blue"
+}
+
+function indigo()
+{
+  document.getElementById('paragraph').style.color="indigo"
+}
+
+function violet()
+{
+  document.getElementById('paragraph').style.color="violet"
+}
